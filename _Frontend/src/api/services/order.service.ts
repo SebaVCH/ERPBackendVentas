778fed0ab9ac7a53826924cc611064402a3870dd
GabrielEtua <gabriel.vergara@alumnos.ucn.cl>
2025-12-09 02:29:06 -0300

    Feat: Order Detail by User

--- _Frontend/src/api/services/order.service.ts
+++ _Frontend/src/api/services/order.service.ts
@@ -1,5 +1,5 @@
 import type { Order, OrderDetail } from "../../types/Order";
-import { adapterOrderDetailResponse, adapterOrderResponse, type OrderDetailResponse } from "../adapter/order.adapter";
+import { adapterOrderDetailResponse, adapterOrderResponse, type OrderDetailResponse, type OrderResponse } from "../adapter/order.adapter";
 import { axiosInstance } from "./axiosInstance";
 
 
@@ -9,6 +9,19 @@ const RESOURCE_NAME = "sales"
 export async function getOrder( orderID : number) : Promise<Order> {
     const { data } = await axiosInstance.get(`${RESOURCE_NAME}/${orderID}`)
     return adapterOrderResponse(data?.data)
+}
+
+export async function getOrdersByClientID(clientID: number): Promise<OrderDetail[]> {
+    const { data } = await axiosInstance.get<{data: OrderResponse[]}>(`${RESOURCE_NAME}/client/${clientID}`)
+
+    if (!Array.isArray(data.data)) return []
+    const orderDetailsPromises = data.data.map((orderData) => 
+        getOrderDetail(orderData.id_venta)
+    )
+    
+    const orderDetails = await Promise.all(orderDetailsPromises)
+    
+    return orderDetails
 }
 
 export async function getOrderDetail(orderID: number): Promise<OrderDetail> {
